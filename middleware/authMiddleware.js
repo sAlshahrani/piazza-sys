cf91a46538deb4c967f7502f74a3a54faa02d33e
sAlshahrani <149010035+sAlshahrani@users.noreply.github.com>
2024-12-11 16:07:26 +0000

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+// Middleware to authenticate requests
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication required' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
